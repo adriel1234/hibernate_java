eb62dfff2a8fe61796be18d1019d378a61700051
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-02-01 20:11:55 -0400

    dao salvar testado

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -11,12 +11,21 @@ public class UsuarioPessoa {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private int idade;
 	private String nome;
 	private String sobrenome;
 	private String email;
 	private String login;
 	private String senha;
 	
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 	public String getEmail() {
 		return email;
 	}
--- src/test/java/mavenhibernate/TesteHibernate.java
+++ src/test/java/mavenhibernate/TesteHibernate.java
@@ -2,11 +2,26 @@ package mavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 	
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setIdade(26);
+		pessoa.setLogin("asdfasdf");
+		pessoa.setNome("Adriel 2");
+		pessoa.setSenha("1234");
+		pessoa.setSobrenome("santos");
+		pessoa.setEmail("asf@fsadf");
+		
+		daoGeneric.salvar(pessoa);
+		
 	}
 
 }
